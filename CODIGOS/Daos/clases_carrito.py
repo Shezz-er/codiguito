b6f058b2e6015faf4d92e4e09e625ede8a34151f
MitoRoss <103219613+MitoRoss@users.noreply.github.com>
2022-07-13 00:54:13 -0400

    Update clases_carrito.py

--- CODIGOS/Daos/clases_carrito.py
+++ CODIGOS/Daos/clases_carrito.py
@@ -2,72 +2,72 @@ from dataclasses import dataclass
 
 @dataclass
 class Vendedor:
-    _id: int
-    _rut: str
-    _nombre: str
-    _apellido: str
-    _telefono: int
-    _direccion: str
-    _email_user: str
-    _password: str
+    id: int
+    rut: str
+    nombre: str
+    apellido: str
+    telefono: int
+    direccion: str
+    email_user: str
+    password: str
 
 @dataclass
 class Cliente:
-    _rut: int
-    _razon: str
-    _giro: str
-    _direccion: str
+    rut: int
+    razon: str
+    giro: str
+    direccion: str
 
 @dataclass
 class Producto:
-    _cod_prod: int
-    _nom_prod: str
-    _valor: int
+    cod_prod: int
+    nom_prod: str
+    valor: int
 
 @dataclass
 class Tipo_doc:
-    _iddoc: int
-    _tipo_documento: str
+    iddoc: int
+    tipo_documento: str
 
 @dataclass
 class Pedido:
-    _idpedido: int
-    _vendedor: Vendedor
-    _detalle: list
+    idpedido: int
+    vendedor: Vendedor
+    detalle: list
 
 @dataclass
 class Boleta:
-    _pedido:Pedido
-    _total:int
+    pedido:Pedido
+    total:int
 
 @dataclass
 class Factura:
-    _pedido:Pedido
-    _cliente:Cliente
-    _neto:int
-    _iva:int
-    _total:int
+    pedido:Pedido
+    cliente:Cliente
+    neto:int
+    iva:int
+    total:int
 
 @dataclass
 class Registro_detalle:
-    _iddt: int
-    _cod_prod: int
-    _idpedido: int
-    _cantidad: int
+    iddt: int
+    cod_prod: int
+    idpedido: int
+    cantidad: int
 
 @dataclass
 class Login:
-    _id:int
-    _password:str
+    id:int
+    password:str
 
 @dataclass
 class Persona:
-    _idpersona: int
-    _rut: str
-    _nombre_persona: str
-    _apellido: str
-    _telefono: int
-    _direccion: str
-    _email_user: str
-    _password: str
-    _idrol: int
+    idpersona: int
+    rut: str
+    nombre_persona: str
+    apellido: str
+    telefono: int
+    direccion: str
+    email_user: str
+    password: str
+    idrol: int
