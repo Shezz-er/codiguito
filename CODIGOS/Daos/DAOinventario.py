f88ac3da3251fb18df3dc021f29ae8052a95180e
diegoaereo <diego.aedo08@inacapmail.cl>
2022-07-12 23:56:46 -0400

    Update DAOinventario.py

--- CODIGOS/Daos/DAOinventario.py
+++ CODIGOS/Daos/DAOinventario.py
@@ -4,16 +4,7 @@ from clases_carrito import *
 
 conexion = mysql.connector.connect(database= "sistema_de_ventas", user="root")
 cursor = conexion.cursor()
-'''
-atributo="123"
-conexion = mysql.connector.connect(database= "sistema_de_ventas", user="root")
-cursor = conexion.cursor()
-query="SELECT cod_prod, nom_prod, precio_prod FROM inventario WHERE cod_prod=CAST(%s AS UNSIGNED INT) OR nom_prod=%s ;"
-cursor.execute(query,(atributo,atributo))
-resultado=cursor.fetchone()
 
-print(resultado)
-'''
 
 def obtenerProducto(atributo):
     query="SELECT cod_prod, nom_prod, precio_prod FROM inventario WHERE cod_prod=CAST(%s AS UNSIGNED INT) OR nom_prod=%s ;"
